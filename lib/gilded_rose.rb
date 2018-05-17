a2c23e41e71c50d1710c9cd533a1a3a3b94bfe31
Jillian Kovalcson <jillian@getreviewbox.com>
2018-05-17 14:22:03 -0400

    Jillian's edits and first refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,50 +6,56 @@ class GildedRose
     @days_remaining = days_remaining
     @quality = quality
   end
-
+  
+  #<insert description of function tick here, including proper use cases, no need to describe inputs>
   def tick
+
+    effect = 0
+
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
+	  if @name != "Conjured Mana Cake"
+            effect = -1
+	  else
+	    if @quality > 1
+	      effect = -2
+	    end
+	  end
         end
       end
     else
       if @quality < 50
-        @quality = @quality + 1
+        effect = 1
         if @name == "Backstage passes to a TAFKAL80ETC concert"
           if @days_remaining < 11
             if @quality < 50
-              @quality = @quality + 1
+              effect = effect + 1
             end
           end
           if @days_remaining < 6
             if @quality < 50
-              @quality = @quality + 1
+              effect = effect + 1 
             end
           end
         end
       end
     end
+
     if @name != "Sulfuras, Hand of Ragnaros"
       @days_remaining = @days_remaining - 1
     end
+
     if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
+      if @name != "Backstage passes to a TAFKAL80ETC concert"
+	if @quality < 49 #needs to check for 49 so that Aged Brie doesn't go over 50
+	  effect = effect * 2
+	end
       else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+	effect = -1 * @quality
       end
     end
+
+    @quality = @quality + effect
   end
 end
